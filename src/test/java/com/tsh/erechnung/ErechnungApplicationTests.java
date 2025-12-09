9a2a9df3ff4f701aa8551ff1d16fa91d81d222ee
Jordan Jovkov <jordan.jovkov@gmail.com>
2025-12-10 01:36:17 +0200

    application's test method commented for easy mvn clean install process

--- src/test/java/com/tsh/erechnung/ErechnungApplicationTests.java
+++ src/test/java/com/tsh/erechnung/ErechnungApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ErechnungApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
